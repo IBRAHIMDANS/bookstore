9fa6c7a4a87996f5df8c430156eca0199596ae99
Ibrahima DANSOKO <ibrahima.Dansoko@outlook.com>
2023-02-23 11:17:30 +0100

    feat: add commit lint & upgrade eslint

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-case': [2, 'always', 'sentence-case'],
-    'body-max-line-length': [1, 'always', 100],
+    'body-max-line-length': [1, 'always', 72],
     'header-max-length': [1, 'always', 52],
     'type-enum': [
       2,
