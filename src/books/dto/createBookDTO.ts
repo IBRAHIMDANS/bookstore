e7904a68c767bcb2c40c4ed2205b8fa669151c64
Ibrahima DANSOKO <ibrahima.Dansoko@outlook.com>
2023-02-23 13:40:10 +0100

    chore: update  prettier

--- src/books/dto/createBookDTO.ts
+++ src/books/dto/createBookDTO.ts
@@ -1,25 +1,41 @@
-import { IsInt, IsNumber, IsOptional, IsString, Length } from 'class-validator';
+import { IsInt, IsNumber, IsOptional, IsString, Length } from "class-validator"
+import { ApiProperty } from "@nestjs/swagger"
 
 export class CreateBookDTO {
+  @ApiProperty({ required: false, description: "Book's id (optional)" })
   @IsNumber()
   @IsOptional()
-  id?: number;
+  id?: number
 
+  @ApiProperty({
+    required: true,
+    description: "Book's title",
+    default: "The Lord of the Rings",
+    minLength: 3,
+    maxLength: 200,
+  })
   @IsString()
   @Length(3, 200)
-  title: string;
+  title: string
 
+  @ApiProperty({ required: true, description: "Book's author" })
   @IsString()
   @Length(3, 65)
-  author: string;
+  author: string
 
+  @ApiProperty({
+    required: false,
+    description: "Book's publication year (optional)",
+    default: 2023,
+  })
   @IsNumber()
   @IsOptional()
   @IsInt()
-  plublicationYear?: number;
+  plublicationYear?: number
 
+  @ApiProperty({ required: false, description: "Book's genre (optional)" })
   @IsString()
   @IsOptional()
   @Length(3, 65)
-  genre: string;
+  genre: string
 }
