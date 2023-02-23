92bd834f84491d734d311346e2e5d95fcbd3d1fb
Ibrahima DANSOKO <ibrahima.Dansoko@outlook.com>
2023-02-23 12:00:01 +0100

    feat: add prisma

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,19 +1,36 @@
-import { Body, Controller, Get, Post, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  ValidationPipe,
+} from '@nestjs/common';
 import { BooksService } from './books.service';
 import { CreateBookDTO } from './dto/createBookDTO';
 
 @Controller('books')
 export class BooksController {
-  constructor(private bookService: BooksService) {
-  }
+  constructor(private bookService: BooksService) {}
 
   @Get()
   findAll() {
     return this.bookService.findAll();
   }
 
+  @Get('/:id')
+  findById(@Param('id') id: number) {
+    return this.bookService.findById(id);
+  }
+
   @Post()
   create(@Body(ValidationPipe) book: CreateBookDTO) {
     return this.bookService.create(book);
+  }
+
+  @Delete()
+  delete(@Param('id') id: number) {
+    return this.bookService.delete(id);
   }
 }
