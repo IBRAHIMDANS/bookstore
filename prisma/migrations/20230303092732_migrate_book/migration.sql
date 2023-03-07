c08e65c028ab6d65f99add5438ef6235534421fd
Ibrahima DANSOKO <ibrahima.Dansoko@outlook.com>
2023-03-07 14:39:46 +0100

    fix: docker for deploy

--- prisma/migrations/20230303092732_migrate_book/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Book" ALTER COLUMN "color" DROP NOT NULL;
--- prisma/migrations/20230306104451_fix_book_modl/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `language` on table `Book` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "Book" ALTER COLUMN "description" SET DEFAULT 'No description',
-ALTER COLUMN "language" SET NOT NULL;
